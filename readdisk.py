80263ea59876b24995eceaf93201a94108727817
owenlo <owenlo@gmail.com>
2018-04-09 18:43:06 +0100

    First commit

---
+++ readdisk.py
@@ -0,0 +1,27 @@
+import os
+
+
+def main():  # Read the first sector of the first disk as example.
+    if os.name == "nt":
+        # Windows based OS normally uses '\\.\physicaldriveX' for disk drive identification.
+        print(read_sector(r"\\.\physicaldrive0"))
+    else:
+        # Linux based OS normally uses '/dev/diskX' for disk drive identification.
+        print(read_sector("/dev/disk0"))
+
+
+def read_sector(disk, sector_no=0):
+    """Read a single sector of the specified disk.
+
+    Keyword arguments:
+    disk -- the physical ID of the disk to read.
+    sector_no -- the sector number to read (default: 0).
+    """
+    f = open(disk, 'rb')
+    f.seek(sector_no * 512)
+    read = f.read(512)
+    return read
+
+
+if __name__ == "__main__":
+    main()
